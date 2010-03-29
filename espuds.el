6bbf797a26513c3798330cdc0006f578f191460c
Johan Andersson <johan.rejeep@gmail.com>
2010-03-29 13:46:21 +0000

    Added some basic steps.

--- espuds.el
+++ espuds.el
@@ -31,6 +31,101 @@
 
 ;;; Code:
 
+(require 'edmacro)
+(require 'cl)
+
+
+(defvar espuds-action-chain '()
+  "List of actions to execute.")
+
+(defvar espuds-chain-active nil
+  "t if chaining is active, nil otherwise.")
+
+
+(Given "^I am in the buffer \"\\(.+\\)\"$"
+       (lambda (buffer)
+         (switch-to-buffer (get-buffer-create buffer))))
+
+(Given "^the buffer is empty$"
+       (lambda ()
+         (erase-buffer)))
+
+(When "I insert this contents \"\\(.+\\)\""
+      (lambda (contents)
+        (insert contents)))
+
+(When "^I insert this contents:$"
+      (lambda (contents)
+        (insert contents)))
+
+(When "^I select \"\\(.+\\)\"$"
+      (lambda (text)
+        (goto-char (point-min))
+        (re-search-forward text)
+        (set-mark (point))
+        (re-search-backward text)))
+
+(When "^I start an action chain$"
+      (lambda ()
+        (setq espuds-action-chain '())
+        (setq espuds-chain-active t)))
+
+(When "^I execute the action chain$"
+      (lambda ()
+        (execute-kbd-macro espuds-action-chain)
+        (setq espuds-chain-active nil)))
+
+(When "^I press \"\\(.+\\)\"$"
+      (lambda (keybinding)
+        (if espuds-chain-active
+            (setq espuds-action-chain (vconcat espuds-action-chain (edmacro-parse-keys keybinding)))
+          (execute-kbd-macro (edmacro-parse-keys keybinding)))))
+
+(Given "^I type \"\\(.+\\)\"$"
+       (lambda (typing)
+         (if espuds-chain-active
+             (setq espuds-action-chain (vconcat espuds-action-chain (string-to-vector typing)))
+           (execute-kbd-macro (string-to-vector typing)))))
+
+(Then "^I should\\( not \\| \\)see \"\\(.+\\)\"$"
+      (lambda (see contents)
+        (let ((hit (search contents (espuds-buffer-contents))))
+          (assert
+           (if (string-match-p "not" see) (not hit) hit)))))
+
+(Then "^I should\\( not \\| \\)see pattern \"\\(.+\\)\"$"
+      (lambda (see contents)
+        (let ((hit (string-match-p contents (espuds-buffer-contents))))
+          (assert
+           (if (string-match-p "not" see) (not hit) hit)))))
+
+(Given "^there is no region selected$"
+       (lambda ()
+         (deactivate-mark)))
+
+(Given "^transient mark mode is active$"
+       (lambda ()
+         (transient-mark-mode 1)))
+
+(Given "^the following is loaded:$"
+       (lambda (contents)
+         (espuds-fake-eval contents)))
+
+(When "I am in the temp file \"\\(.+\\)\""
+      (lambda (file)
+        (find-file (make-temp-file file))))
+
+(defun espuds-fake-eval (contents)
+  "Dumps contents to a temp file and then loads it."
+  (let ((file (make-temp-file "ecukes-")))
+    (with-temp-file file
+      (insert contents))
+    (load file)))
+
+(defun espuds-buffer-contents ()
+  "Returns all text in current buffer."
+  (buffer-substring-no-properties (point-min) (point-max)))
+
 (provide 'espuds)
 
 ;;; espuds.el ends here
