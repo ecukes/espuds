8efd306de7ea5e9e9f60593667c77e5ae434ba81
Johan Andersson <johan.rejeep@gmail.com>
2012-04-15 10:45:18 +0200

    Merge two steps that are actually the same.

--- espuds-buffer.el
+++ espuds-buffer.el
@@ -1,11 +1,12 @@
 ;;; espuds-buffer.el --- Buffer related definitions
 
 
-;; Switches buffer in the current window to BUFFER.
+;; Switches to BUFFER.
 ;;
 ;; Usage:
+;;   When I switch to buffer "Foo"
 ;;   Given I am in buffer "*scratch*"
-(Given "^I am in buffer \"\\(.+\\)\"$"
+(Given "^\\(?:I am in buffer\\|I switch to buffer\\) \"\\(.+\\)\"$"
        (lambda (buffer)
          (let ((v (vconcat [?\C-x ?b] (string-to-vector buffer))))
            (execute-kbd-macro v))))
@@ -40,15 +41,6 @@
 (Given "^the buffer is empty$\\|^I clear the buffer$"
        (lambda ()
          (erase-buffer)))
-
-;; Switches to BUFFER.
-;;
-;; Usage:
-;;   When I switch to buffer "Foo"
-(When "^I switch to buffer \"\\(.+\\)\"$"
-      (lambda (buffer)
-        (let ((v (vconcat [?\C-x ?b] (string-to-vector buffer))))
-          (execute-kbd-macro v))))
 
 
 (provide 'espuds-buffer)
