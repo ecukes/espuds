9f19a58db655819412740f19ecfdaaf4774571a4
Johan Andersson <johan.rejeep@gmail.com>
2013-08-31 21:55:44 +0200

    Release v0.2.3.

--- espuds.el
+++ espuds.el
@@ -4,7 +4,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.2.2
+;; Version: 0.2.3
 ;; Keywords: test
 ;; URL: http://github.com/rejeep/espuds
 
