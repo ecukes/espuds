84ab82dbd90ceb00319af116e5cf09090b3aabe1
Damien Cassou <damien.cassou@gmail.com>
2014-03-04 14:15:06 +0100

    Remove useless `with-mock'

--- test/espuds-test.el
+++ test/espuds-test.el
@@ -729,9 +729,8 @@
 (ert-deftest when-i-delete-other-windows ()
   "Should kill all windows except 1."
   (with-playground
-   (with-mock
-    (should (equal 1 (count-windows)))
-    (split-window-right)
-    (should (equal 2 (count-windows)))
-    (When "I delete other windows")
-    (should (equal 1 (count-windows))))))
+   (should (equal 1 (count-windows)))
+   (split-window-right)
+   (should (equal 2 (count-windows)))
+   (When "I delete other windows")
+   (should (equal 1 (count-windows)))))
